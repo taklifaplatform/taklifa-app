d070f89ee1f5d476e4f2d56a6245396347a84e0b
Badi Ifaoui <badi1wow@gmail.com>
2024-01-13 12:06:58 +0100

    fix: theme builder

--- libs/core/theme/src/lib/tamagui.config.ts
+++ libs/core/theme/src/lib/tamagui.config.ts
@@ -1,7 +1,21 @@
-import { createTamagui } from '@tamagui/core';
+import { config } from '@tamagui/config/v2';
+import { createTamagui } from '@tamagui/core'; // or '@tamagui/core'
 import { themes } from './theme-builder';
-
+/**
+ * config:
+ * - defaultFont
+ * - shouldAddPrefersColorThemes
+ * - themeClassNameOnRoot
+ * - themes
+ * - media
+ * - shorthands
+ * - tokens
+ * - fonts
+ * - mediaQueryDefaultActive
+ * - selectionStyles
+ * - animations
+ */
 export const themeConfig = createTamagui({
-  // tokens,
+  ...config,
   themes,
 });
