2c2a97111d3b35d1c5d516d087df61aaae80027c
Badi Ifaoui <badi1wow@gmail.com>
2024-01-14 18:56:29 +0100

    fix: root theme

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -1,4 +1,11 @@
-import type { SolitoAppProps } from 'solito';
+import '@tamagui/core/reset.css';
+import '@tamagui/font-inter/css/400.css';
+import '@tamagui/font-inter/css/700.css';
+import {
+  ColorScheme,
+  NextThemeProvider,
+  useRootTheme
+} from '@tamagui/next-theme';
 
 import { api } from '@zix/app/api';
 import {
@@ -7,7 +14,14 @@ import {
 } from '@zix/app/providers/website';
 import { NextPage } from 'next';
 import Head from 'next/head';
+
+import 'raf/polyfill';
 import { ReactElement, ReactNode } from 'react';
+import type { SolitoAppProps } from 'solito';
+
+if (process.env.NODE_ENV === 'production') {
+  require('../public/tamagui.css');
+}
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -18,6 +32,8 @@ const ZixApp: React.FC<
 > = ({ Component, pageProps }) => {
   // reference: https://nextjs.org/docs/pages/building-your-application/routing/pages-and-layouts
   const getLayout = Component.getLayout || ((page) => page);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_theme, setTheme] = useRootTheme();
 
   return (
     <>
@@ -26,9 +42,15 @@ const ZixApp: React.FC<
         <meta name="description" content="ZIX Core" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <WebsiteAppProvider initialSession={pageProps.initialSession}>
-        {getLayout(<Component {...pageProps} />)}
-      </WebsiteAppProvider>
+      <NextThemeProvider
+        onChangeTheme={(next) => {
+          setTheme(next as ColorScheme);
+        }}
+      >
+        <WebsiteAppProvider initialSession={pageProps.initialSession}>
+          {getLayout(<Component {...pageProps} />)}
+        </WebsiteAppProvider>
+      </NextThemeProvider>
     </>
   );
 };
