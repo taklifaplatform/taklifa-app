d64fdd95ded32212950eaaf41e9af8e9c41b55fc
Badi Ifaoui <badi1wow@gmail.com>
2024-01-14 18:05:38 +0100

    feat: initial website storybook

---
+++ apps/website/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: [
+    '../../../libs/app/ui/**/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    // '../../../libs/ui/**/src/**/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    // '../../../libs/ui/**/src/**/**/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
