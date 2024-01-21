843e53628255c9ae9ada772c9f21d5d3f79c82d5
Badi Ifaoui <badi1wow@gmail.com>
2024-01-21 08:57:54 +0100

    fix: home cars screen

--- libs/app/features/customer-dashboard/src/screens/home-screen/home-screen.tsx
+++ libs/app/features/customer-dashboard/src/screens/home-screen/home-screen.tsx
@@ -1,6 +1,6 @@
 import { ZixMap } from '@zix/app/ui/common';
 import { IMarker, MapVehicleMarker } from '@zix/app/ui/sawaeed';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import type { Region } from 'react-native-maps';
 // import MapView from 'react-native-maps';
 
@@ -9,46 +9,77 @@ export interface HomeScreenProps {}
 
 const vehicleTypes = ['vehicle_a', 'vehicle_b', 'vehicle_c'];
 
+const initialCamera = {
+  center: {
+    latitude: 24.713552,
+    longitude: 46.675296
+  },
+  pitch: 0,
+  heading: 0,
+  altitude: 100000,
+  zoom: 20
+};
+
 export function HomeScreen(props: HomeScreenProps) {
   // create random 10 dynamically moving markers on the map
   const [markers, setMarkers] = useState<IMarker[]>([]);
   const [currentRegion, setCurrentRegion] = useState<Region>();
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // move markers
-      setMarkers((prev) =>
-        prev.map((marker) => ({
-          ...marker,
-          rotateDegree: `${Math.random() * 20}deg`,
-          coordinate: {
-            latitude: marker.coordinate.latitude + (Math.random() - 0.5) * 0.02,
-            longitude:
-              marker.coordinate.longitude + (Math.random() - 0.5) * 0.02
-          }
-        }))
-      );
-    }, 1000);
-    return () => clearInterval(interval);
+
+  const moveMarkers = useCallback(() => {
+    setMarkers((prev) =>
+      prev.map((marker) => ({
+        ...marker,
+        rotateDegree: `${Math.random() * 20}deg`,
+        coordinate: {
+          latitude: marker.coordinate.latitude + (Math.random() - 0.5) * 0.005,
+          longitude: marker.coordinate.longitude + (Math.random() - 0.5) * 0.005
+        }
+      }))
+    );
   }, []);
 
-  function onSetCurrentRegion(region: Region) {
-    if (!currentRegion) {
-      setMarkers(
-        Array.from({ length: 10 }).map(() => ({
-          vehicle_type: vehicleTypes[Math.floor(Math.random() * 3)],
-          coordinate: {
-            latitude: region?.latitude + (Math.random() - 0.5) * 2,
-            longitude: region?.longitude + (Math.random() - 0.5) * 2
-          }
-        }))
-      );
-    }
+  const setInitialMarkers = useCallback(() => {
+    const newMarkers: IMarker[] = [];
 
-    setCurrentRegion(region);
-  }
+    Array.from({ length: 15 }).forEach(() => {
+      newMarkers.push({
+        vehicle_type: vehicleTypes[Math.floor(Math.random() * 3)],
+        coordinate: {
+          latitude:
+            initialCamera.center?.latitude + (Math.random() - 0.5) * 0.2,
+          longitude:
+            initialCamera.center?.longitude + (Math.random() - 0.5) * 0.2
+        }
+      });
+    });
+    Array.from({ length: 40 }).forEach(() => {
+      newMarkers.push({
+        vehicle_type: vehicleTypes[Math.floor(Math.random() * 3)],
+        coordinate: {
+          latitude:
+            initialCamera.center?.latitude + (Math.random() - 0.5) * 5,
+          longitude:
+            initialCamera.center?.longitude + (Math.random() - 0.5) * 5
+        }
+      });
+    });
+    setMarkers(newMarkers);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      moveMarkers();
+    }, 1000);
+    setInitialMarkers();
+    return () => clearInterval(interval);
+  }, [moveMarkers]);
 
   return (
-    <ZixMap.MapView style={{ flex: 1 }} onRegionChange={onSetCurrentRegion}>
+    <ZixMap.MapView
+      style={{ flex: 1 }}
+      initialCamera={initialCamera}
+      onRegionChange={setCurrentRegion}
+    >
       {markers.map((marker, index) => (
         <MapVehicleMarker key={`key-${index}`} index={index} marker={marker} />
       ))}
