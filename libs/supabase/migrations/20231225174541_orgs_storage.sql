64a8274ec169a2f3a8d8c5f09f9da0faf59f8ea1
Badi Ifaoui <badi1wow@gmail.com>
2024-01-22 10:43:05 +0100

    feat: org storage

--- libs/supabase/migrations/20231225174541_orgs_storage.sql
+++ libs/supabase/migrations/20231225174541_orgs_storage.sql
@@ -1,3 +1,80 @@
 insert into storage.buckets (id, name, public)
 values ('orgs', 'orgs', false)
 on conflict do nothing;
+
+create schema if not exists private;
+
+create policy "Only allow users to read storage from org buckets they're member on or the public files"
+on storage.objects
+as permissive
+for select
+to public
+using (
+  (
+    (bucket_id = 'orgs'::text)
+    AND (storage.foldername(name))[2]::text = 'public'::text
+    OR EXISTS (
+      SELECT 1
+      FROM public.org_memberships
+      WHERE org_id = (storage.foldername(name))[1]::uuid
+      AND user_id = auth.uid()::uuid
+    )
+  )
+);
+
+
+create policy "Only allow users to write storage from org buckets they're member on"
+on storage.objects
+as permissive
+for insert
+to public
+with check (
+  (
+    (bucket_id = 'chat'::text)
+    AND EXISTS (
+      SELECT 1
+      FROM public.org_memberships
+      WHERE org_id = (storage.foldername(name))[1]::uuid
+      AND user_id = auth.uid()::uuid
+    )
+  )
+);
+
+
+create policy "Only allow users to delete storage from org buckets they're member on"
+on storage.objects
+as permissive
+for delete
+to public
+using (
+  (
+    (bucket_id = 'chat'::text)
+    AND EXISTS (
+      SELECT 1
+      FROM public.org_memberships
+      WHERE org_id = (storage.foldername(name))[1]::uuid
+      AND user_id = auth.uid()::uuid
+    )
+  )
+);
+
+
+create policy "Only allow users to update storage from org buckets they're member on"
+on storage.objects
+as permissive
+for update
+to public
+using (
+  (
+    (bucket_id = 'chat'::text)
+    AND EXISTS (
+      SELECT 1
+      FROM public.org_memberships
+      WHERE org_id = (storage.foldername(name))[1]::uuid
+      AND user_id = auth.uid()::uuid
+    )
+  )
+);
+
+
+
