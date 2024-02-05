9a561dc861af755b0a43af5ebb0643753732ecdf
Badi Ifaoui <badi.ifaoui@247labs.com>
2024-02-05 07:37:50 +0100

    feat: test

--- apps/mobile/src/app/customer/(tabs)/explore/index.tsx
+++ apps/mobile/src/app/customer/(tabs)/explore/index.tsx
@@ -1,8 +1,17 @@
+import { api } from '@zix/api';
 import { AppHeader } from '@zix/app/ui/common';
+import { View, Text } from '@zix/app/ui/core';
 import React from 'react';
 
 export default function Screen() {
+  const query = api.countries.get.useQuery()
   return (
-    <AppHeader showBackButton title="Chat" />
+    <>
+      <AppHeader showBackButton title="Chat" />
+      <View>
+        <Text>Chat</Text>
+        <Text>query:: {JSON.stringify(query, null, 2)}</Text>
+      </View>
+    </>
   );
 }
