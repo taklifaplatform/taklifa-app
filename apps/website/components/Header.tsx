18dba99b697c29a6200a6365e9d5618f059818a7
Salah ifaoui <salah2022@gmail.com>
2024-02-26 23:15:27 +0100

    feat: fix header icon space

--- apps/website/components/Header.tsx
+++ apps/website/components/Header.tsx
@@ -9,74 +9,48 @@ export function Header() {
     <XStack
       $sm={{ display: 'none' }}
       justifyContent="space-between"
-      w={'100%'}
       backgroundColor={'$color1'}
       alignItems="center"
-      paddingVertical="$4"
       borderBottomLeftRadius={20}
       borderBottomRightRadius={20}
       borderTopWidth={2}
       borderTopColor={'$gray7'}
+      marginBottom="$4"
+      paddingHorizontal="$4"
+      paddingVertical="$0"
     >
-      <XStack w={'45%'} space="$4" paddingHorizontal="$4">
+      <XStack gap="$3">
         <Button
-          backgroundColor="transparent"
-          paddingVertical="$4"
+        backgroundColor={'transparent'}
+          borderColor={'$color5'}
           onPress={() => {}}
           icon={<CustomIcon name={'account'} size="$2" />}
         >
           <Text color="$white">{t('web-home:signup')}</Text>
         </Button>
-        <Button
-          backgroundColor="transparent"
-          paddingVertical="$4"
-          onPress={() => {}}
-          icon={<CustomIcon name={'rigning'} size="$2" />}
-        >
-          <Text color="$white">{t('web-home:call')}</Text>
+        <Button backgroundColor="transparent" onPress={() => {}}>
+          <Text>{t('web-home:call')}</Text>
+          <CustomIcon name={'rigning'} size="$1" />
         </Button>
-        <Button
-          backgroundColor="transparent"
-          paddingVertical="$4"
-          onPress={() => {}}
-          icon={<CustomIcon name={'search'} size="$2" />}
-        >
-          <Text color="$white">{t('web-home:search')}</Text>
+        <Button backgroundColor="transparent" onPress={() => {}}>
+        <Text>{t('web-home:search')}</Text>
+          <CustomIcon name={'search'} size="$1" />
         </Button>
       </XStack>
-      <XStack w={'10%'}>
-        <CustomIcon name={'weblogo'} width="$15" height="$8" />
+      <XStack>
+        <CustomIcon name={'weblogo'} size={'$11'} />
       </XStack>
-      <XStack
-        w={'45%'}
-        justifyContent="flex-end"
-        space="$4"
-        paddingHorizontal="$4"
-      >
-        <Button
-          backgroundColor={'transparent'}
-          paddingVertical="$4"
-          onPress={() => {}}
-        >
-          <Text color="$white">{t('web-home:works')}</Text>
+      <XStack gap="$3">
+        <Button backgroundColor={'transparent'} onPress={() => {}}>
+        <Text>{t('web-home:works')}</Text>
         </Button>
-        <Button
-          backgroundColor={'transparent'}
-          paddingVertical="$4"
-          onPress={() => {}}
-        >
-          <Text color="$white">{t('web-home:followers')}</Text>
+        <Button backgroundColor={'transparent'} onPress={() => {}}>
+        <Text>{t('web-home:followers')}</Text>
         </Button>
-        <Button
-          backgroundColor={'transparent'}
-          paddingVertical="$4"
-          onPress={() => {}}
-        >
-          <Text color="$white">{t('web-home:payments')}</Text>
+        <Button backgroundColor={'transparent'} onPress={() => {}}>
+        <Text>{t('web-home:payments')}</Text>
         </Button>
-        <Button paddingVertical="$4" onPress={() => {}}>
-          <Text color="$white">{t('web-home:home')}</Text>
-        </Button>
+        <Button onPress={() => {}}><Text>{t('web-home:home')}</Text></Button>
       </XStack>
     </XStack>
   );
