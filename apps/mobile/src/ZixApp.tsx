1bcc7dfecabd04f0f3b8c2b30306a20686280876
Badi Ifaoui <badi.ifaoui@zixdev.com>
2024-03-05 11:30:32 +0100

    fix: date picker

--- apps/mobile/src/ZixApp.tsx
+++ apps/mobile/src/ZixApp.tsx
@@ -3,12 +3,6 @@ import { bootMultiLang, MultiLangAppProvider } from '@zix/i18n'
 import { ExpoRoot } from 'expo-router';
 import React from 'react';
 
-/**
- * Need to load them here to avoid the following error:
- * package not found when sync with expo
- */
-import "react-native-modal-datetime-picker";
-
 
 // import { LogBox } from 'react-native'
 
--- libs/ui/forms/src/fields/zix-date-fields/zix-date-picker.native.tsx
+++ libs/ui/forms/src/fields/zix-date-fields/zix-date-picker.native.tsx
@@ -1,11 +1,11 @@
-import { CalendarDays } from '@tamagui/lucide-icons'
-import moment from 'moment'
-import { useState } from 'react'
-import DateTimePickerModal from 'react-native-modal-datetime-picker'
-import { View } from 'tamagui'
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { CalendarDays } from '@tamagui/lucide-icons';
+import moment from 'moment';
+import { useState } from 'react';
+import { Button, Sheet, View, YStack } from 'tamagui';
 
-import ZixInput from '../zix-input/zix-input'
-import { ZixDateFieldProps } from './types'
+import ZixInput from '../zix-input/zix-input';
+import { ZixDateFieldProps } from './types';
 
 export const ZixDatePicker: React.FC<ZixDateFieldProps> = ({
   onChange,
@@ -32,18 +32,34 @@ export const ZixDatePicker: React.FC<ZixDateFieldProps> = ({
         />
       </View>
 
-      {startOpen && (
-        <DateTimePickerModal
-          date={moment(value ?? undefined).toDate()}
-          isVisible={startOpen}
-          mode={'date'}
-          onConfirm={(date) => {
-            onChange?.(moment(date).format('YYYY-MM-DD'))
-            startOpenChange(false)
-          }}
-          onCancel={() => startOpenChange(false)}
-        />
-      )}
+      <Sheet
+        open={startOpen}
+        onOpenChange={startOpenChange}
+        dismissOnSnapToBottom
+        native
+        modal
+        snapPoints={[60, 70]}
+      >
+        <Sheet.Overlay />
+        <Sheet.Handle />
+        <Sheet.Frame>
+          <YStack padding='$4'>
+
+            <DateTimePicker
+              textColor='red'
+              value={moment(value ?? undefined).toDate()}
+              mode={'date'}
+              display='inline'
+              onChange={(date) => {
+                onChange?.(moment(date).format('YYYY-MM-DD'))
+              }}
+            />
+            <Button themeInverse onPress={() => startOpenChange(false)}>
+              Confirm
+            </Button>
+          </YStack>
+        </Sheet.Frame>
+      </Sheet>
     </>
   )
 }
