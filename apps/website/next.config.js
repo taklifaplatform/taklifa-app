42227fb9044cea3e0fa7d822111945d2e5a53354
Badi Ifaoui <badi.ifaoui@zixdev.com>
2024-03-08 09:23:03 +0100

    fix: tg issues

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -61,10 +61,10 @@ const nextConfig = {
 
 const tamaguiPlugin = withTamagui({
   platform: 'web',
-  themeBuilder: {
-    input: '../../libs/theme/src/lib/sawaeed-theme/themes/theme.ts',
-    output: '../../libs/theme/src/lib/sawaeed-theme/themes.ts',
-  },
+  // themeBuilder: {
+  //   input: '../../libs/theme/src/lib/sawaeed-theme/themes/theme.ts',
+  //   output: '../../libs/theme/src/lib/sawaeed-theme/themes.ts',
+  // },
 
   config: './tamagui.config.ts',
   components: ['tamagui'],
@@ -73,7 +73,7 @@ const tamaguiPlugin = withTamagui({
   outputCSS: process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
 
 
-  disableExtraction: true
+  // disableExtraction: true
 })
 
 const plugins = [
