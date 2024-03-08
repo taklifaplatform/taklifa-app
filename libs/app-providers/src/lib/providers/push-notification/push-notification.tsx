e874c66c182e7ba5613610565107d6ba9a99a8c1
Badi Ifaoui <badi.ifaoui@zixdev.com>
2024-03-08 14:06:05 +0100

    fix: expo push notification is not supported on web

--- libs/app-providers/src/lib/providers/push-notification/push-notification.tsx
+++ libs/app-providers/src/lib/providers/push-notification/push-notification.tsx
@@ -1,17 +1,4 @@
-
-import { useAuth } from '@zix/utils';
-import React, { useEffect } from 'react';
-import { registerForPushNotificationsAsync } from './registerForPushNotificationsAsync';
-import { NotificationService } from '@zix/api';
-import * as Notifications from 'expo-notifications';
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-  }),
-});
+import React from 'react';
 
 export type PushNotificationProps = {
   children: React.ReactNode;
@@ -20,24 +7,6 @@ export type PushNotificationProps = {
 export const PushNotification: React.FC<PushNotificationProps> = ({
   children
 }) => {
-  const { authAccessToken } = useAuth()
-
-  async function registerPushNotificationToken() {
-    const token = await registerForPushNotificationsAsync()
-
-    if (token && authAccessToken) {
-      NotificationService.storeExpoToken({
-        requestBody: {
-          token
-        }
-      })
-    }
-  }
-
-  useEffect(() => {
-    registerPushNotificationToken()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authAccessToken])
   return children;
 }
 
