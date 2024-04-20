369df65fb07279415263ce4efbff0bc2fe1264e0
“Badi <badi1wow@gmail.com>
2024-04-20 13:33:49 +0100

    feat: auto icons importer script

---
+++ libs/ui/icons/file.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+dir="./libs/ui/icons/src/assets/icons"
+output_dir="./libs/ui/icons/src/assets/"
+content_array=()
+idx=0
+
+
+# Check if the file exists, and if so, delete it
+if [ -f "$output_dir/icon-names.ts" ]; then
+    rm "$output_dir"/icon-names.ts
+    echo "Existing file 'icon-names.ts' has been deleted"
+fi
+if [ -f "$output_dir/index.ts" ]; then
+    rm "$output_dir"/index.ts
+    echo "Existing file 'index.ts' has been deleted"
+fi
+
+# Write the content to a file named "icon-names.ts"
+start_file_icon_name="export const iconNames = ["
+end_file_icon_name="];"
+echo "$start_file_icon_name" > "$output_dir"/icon-names.ts
+
+# Write the content to a file named "index.ts"
+start_file_index="// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck"
+echo "$start_file_index" > "$output_dir"/index.ts
+
+for f in "$dir"/*; do
+filename=$(basename "$f")
+file="${filename%.*}"
+
+# convert fileName '-' to '_'
+file_to_icon_name=$(echo "$file" | sed 's/-\([a-zA-Z]\)/\_\1/g')
+# Write the content to a file named "icon-names.ts"
+echo "  '$file_to_icon_name'," >> "$output_dir"/icon-names.ts
+
+
+# Change the first letter after a hyphen '-' to uppercase
+for (( i=0; i<${#file}; i++ )); do
+    letter="${file:i:1}"
+    if [[ $letter = "-" ]]; then
+       file=${file:0:$i}"$(tr '[:lower:]' '[:upper:]' <<< ${file:$i+1:1})${file:$i+2}"
+    fi
+done
+
+#Change the first letter to uppercase
+foo="$(tr '[:lower:]' '[:upper:]' <<< ${file:0:1})${file:1}"
+# Write the content to a file named "index.ts"
+echo "import $foo from './icons/$filename'; " >> "$output_dir"/index.ts
+
+# Save each word of the content in an array
+content_array[idx]="$foo"
+idx=$((idx+1))
+
+done
+# Print the array elements
+
+echo "
+
+export {" >> "$output_dir"/index.ts
+for element in "${content_array[@]}"; do
+    echo "  $element," >> "$output_dir"/index.ts
+done
+echo "}" >> "$output_dir"/index.ts
+
+
+echo "$end_file_icon_name" >> "$output_dir"/icon-names.ts
+echo "Data has been written to icon-names.ts"
+echo "Data has been written to index.ts"
--- libs/ui/icons/src/assets/index.ts
+++ libs/ui/icons/src/assets/index.ts
@@ -1,162 +1,156 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import About from './icons/about.svg'; 
+import Account from './icons/account.svg'; 
+import AlertCercle from './icons/alert-cercle.svg'; 
+import Alert from './icons/alert.svg'; 
+import AppStore from './icons/app-store.svg'; 
+import Apps from './icons/apps.svg'; 
+import AspectRatio from './icons/aspect-ratio.svg'; 
+import Avatar from './icons/avatar.svg'; 
+import Bolt from './icons/bolt.svg'; 
+import Book from './icons/book.svg'; 
+import BoxAdd from './icons/box-add.svg'; 
+import Call from './icons/call.svg'; 
+import Car from './icons/car.svg'; 
+import Category from './icons/category.svg'; 
+import Chat from './icons/chat.svg'; 
+import Chronic from './icons/chronic.svg'; 
+import Client from './icons/client.svg'; 
+import Comment from './icons/comment.svg'; 
+import CompanyShipping from './icons/company-shipping.svg'; 
+import Company from './icons/company.svg'; 
+import ContactCalendar from './icons/contact-calendar.svg'; 
+import DimensionBox from './icons/dimension-box.svg'; 
+import EmptyChat from './icons/empty-chat.svg'; 
+import EmptyData from './icons/empty-data.svg'; 
+import EmptyFolder from './icons/empty-folder.svg'; 
+import EmptyNotification from './icons/empty-notification.svg'; 
+import Facebook from './icons/facebook.svg'; 
+import FindTrack from './icons/find-track.svg'; 
+import Flip from './icons/flip.svg'; 
+import Followed from './icons/followed.svg'; 
+import Garage from './icons/garage.svg'; 
+import GooglePlay from './icons/google-play.svg'; 
+import HalfStar from './icons/half-star.svg'; 
+import HomeInfo from './icons/home-info.svg'; 
+import Home from './icons/home.svg'; 
+import Instagram from './icons/instagram.svg'; 
+import Job from './icons/job.svg'; 
+import List from './icons/list.svg'; 
+import LocalShipping from './icons/local-shipping.svg'; 
+import LocationCheck from './icons/location-check.svg'; 
+import Location from './icons/location.svg'; 
+import Lock from './icons/lock.svg'; 
+import Logo from './icons/logo.svg'; 
+import Logout from './icons/logout.svg'; 
+import Mail from './icons/mail.svg'; 
+import Map from './icons/map.svg'; 
+import Matricule from './icons/matricule.svg'; 
+import More from './icons/more.svg'; 
+import NearMe from './icons/near-me.svg'; 
+import Notifications from './icons/notifications.svg'; 
+import Opacity from './icons/opacity.svg'; 
+import Orders from './icons/orders.svg'; 
+import PaperMoney from './icons/paper-money.svg'; 
+import RadioButtonChecked from './icons/radio-button-checked.svg'; 
+import Ringing from './icons/ringing.svg'; 
+import SearchTrack from './icons/search-track.svg'; 
+import Search from './icons/search.svg'; 
+import Secure from './icons/secure.svg'; 
+import Settings from './icons/settings.svg'; 
+import Share from './icons/share.svg'; 
+import Shield_with_heart from './icons/shield_with_heart.svg'; 
+import Shipping from './icons/shipping.svg'; 
+import Snapchat from './icons/snapchat.svg'; 
+import Star from './icons/star.svg'; 
+import Store from './icons/store.svg'; 
+import Success from './icons/success.svg'; 
+import Theme from './icons/theme.svg'; 
+import TikTok from './icons/tik-tok.svg'; 
+import Time from './icons/time.svg'; 
+import Touch_app from './icons/touch_app.svg'; 
+import UserInfo from './icons/user-info.svg'; 
+import Wallet from './icons/wallet.svg'; 
+import WebDarkLogo from './icons/web-dark-logo.svg'; 
+import WebLogoAr from './icons/web-logo-ar.svg'; 
+import WebLogoEn from './icons/web-logo-en.svg'; 
 
-import About from './icons/about.svg';
-import Account from './icons/account.svg';
-import Apps from './icons/apps.svg';
-import AppStore from './icons/app-store.svg';
-import Avatar from './icons/avatar.svg';
-import AspectRatio from './icons/aspect-ratio.svg';
-import Book from './icons/book.svg';
-import Call from './icons/call.svg';
-import Car from './icons/car.svg';
-import Chat from './icons/chat.svg';
-import Client from './icons/client.svg';
-import CompanyShipping from './icons/company-shipping.svg';
-import Company from './icons/company.svg';
-import Chronic from './icons/chronic.svg';
-import Comment from './icons/comment.svg';
-import EmptyChat from './icons/empty-chat.svg';
-import EmptyData from './icons/empty-data.svg';
-import EmptyFolder from './icons/empty-folder.svg';
-import EmptyNotification from './icons/empty-notification.svg';
-import Facebook from './icons/facebook.svg';
-import FindTrack from './icons/find-track.svg';
-import Flip from './icons/flip.svg';
-import Followed from './icons/followed.svg';
-import GooglePlay from './icons/google-play.svg';
-import HalfStar from './icons/half-star.svg';
-import Home from './icons/home.svg';
-import Instagram from './icons/instagram.svg';
-import List from './icons/list.svg';
-import Location from './icons/location.svg';
-import Lock from './icons/lock.svg';
-import Logo from './icons/logo.svg';
-import Logout from './icons/logout.svg';
-import Mail from './icons/mail.svg';
-import Map from './icons/map.svg';
-import More from './icons/more.svg';
-import Notifications from './icons/notifications.svg';
-import Orders from './icons/orders.svg';
-import Ringing from './icons/ringing.svg';
-import Search from './icons/search.svg';
-import SearchTrack from './icons/search-track.svg';
-import Secure from './icons/secure.svg';
-import Settings from './icons/settings.svg';
-import Share from './icons/share.svg';
-import Shipping from './icons/shipping.svg';
-import Snapchat from './icons/snapchat.svg';
-import Star from './icons/star.svg';
-import Store from './icons/store.svg';
-import Success from './icons/success.svg';
-import Theme from './icons/theme.svg';
-import TikTok from './icons/tik-tok.svg';
-import Time from './icons/time.svg';
-import Wallet from './icons/wallet.svg';
-import WebLogoAr from './icons/web-logo-ar.svg';
-import WebDarkLogo from './icons/web-dark-logo.svg';
-import WebLogoEn from './icons/web-logo-en.svg';
-import Job from "./icons/job.svg";
-import RadioButtonChecked from "./icons/radio-button-checked.svg";
-import Garage from "./icons/garage.svg";
-import Opacity from "./icons/opacity.svg";
-import Matricule from "./icons/matricule.svg";
-import UserInfo from "./icons/user-info.svg";
-import AlertCercle from "./icons/alert-cercle.svg";
-import Alert from "./icons/alert.svg";
-import Bolt from "./icons/bolt.svg";
-import Touch_app from "./icons/touch_app.svg";
-import Category from "./icons/category.svg";
-import Shield_with_heart from "./icons/shield_with_heart.svg";
-import DimensionBox from "./icons/dimension-box.svg";
-import BoxAdd from "./icons/box-add.svg";
-import LocationCheck from "./icons/location-check.svg";
-import ContactCalendar from "./icons/contact-calendar.svg";
-import PaperMoney from "./icons/paper-money.svg";
-import NearMe from "./icons/near-me.svg";
-import CompanyCars from "./arts/company-cars.svg";
-import LookingForService from "./arts/looking-for-service.svg";
-import ServiceProvider from "./arts/service-provider.svg";
-import SoloTransporterCar from "./arts/solo-transporter-car.svg";
-import VehicleA from "./vehicles/vehicle-a.svg";
 
 export {
   About,
   Account,
-  Apps,
+  AlertCercle,
+  Alert,
   AppStore,
-  Avatar,
+  Apps,
   AspectRatio,
+  Avatar,
+  Bolt,
   Book,
+  BoxAdd,
   Call,
   Car,
+  Category,
   Chat,
-  Client,
-  CompanyCars,
-  CompanyShipping,
-  Comment,
   Chronic,
+  Client,
+  Comment,
+  CompanyShipping,
   Company,
-  EmptyNotification,
+  ContactCalendar,
+  DimensionBox,
   EmptyChat,
   EmptyData,
   EmptyFolder,
+  EmptyNotification,
   Facebook,
   FindTrack,
   Flip,
   Followed,
+  Garage,
   GooglePlay,
   HalfStar,
+  HomeInfo,
   Home,
   Instagram,
   Job,
   List,
+  LocalShipping,
+  LocationCheck,
   Location,
   Lock,
   Logo,
   Logout,
-  Bolt,
-  Touch_app,
-  Category,
-  Shield_with_heart,
-  BoxAdd,
-  LocationCheck,
-  ContactCalendar,
-  PaperMoney,
-  NearMe,
-  LookingForService,
   Mail,
   Map,
-  AlertCercle,
+  Matricule,
   More,
+  NearMe,
   Notifications,
+  Opacity,
   Orders,
+  PaperMoney,
+  RadioButtonChecked,
   Ringing,
-  Search,
   SearchTrack,
+  Search,
   Secure,
-  ServiceProvider,
   Settings,
   Share,
+  Shield_with_heart,
   Shipping,
   Snapchat,
-  Alert,
-  SoloTransporterCar,
   Star,
   Store,
   Success,
   Theme,
   TikTok,
   Time,
-  VehicleA,
-  Wallet,
-  WebLogoAr,
-  WebDarkLogo,
-  WebLogoEn,
-  RadioButtonChecked,
-  Garage,
-  Opacity,
-  Matricule,
+  Touch_app,
   UserInfo,
-  DimensionBox
-};
+  Wallet,
+  WebDarkLogo,
+  WebLogoAr,
+  WebLogoEn,
+}
