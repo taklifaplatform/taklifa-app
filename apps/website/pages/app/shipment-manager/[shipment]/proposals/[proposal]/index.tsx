936d2094ea40ec44ff30bd490192078d975d9855
Choaibe ifaoui <ifaoui.choaibe@gmail.com>
2024-10-29 10:30:29 +0100

    added: Shipment View Request & Proposals & Invitations : Web App

---
+++ apps/website/pages/app/shipment-manager/[shipment]/proposals/[proposal]/index.tsx
@@ -0,0 +1,6 @@
+import { ShipmentProposalDetailScreen } from '@zix/features/shipments';
+import React from 'react';
+
+export default function Screen() {
+  return <ShipmentProposalDetailScreen />;
+}
