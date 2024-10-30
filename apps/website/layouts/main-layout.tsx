5cb1e7548ca4c30d9dc872e6adc6ac7df14190c1
Choaibe ifaoui <ifaoui.choaibe@gmail.com>
2024-10-30 10:48:47 +0100

    added: scroll to top: homescreen

--- apps/website/layouts/main-layout.tsx
+++ apps/website/layouts/main-layout.tsx
@@ -1,7 +1,8 @@
 import { ZixContainer } from '@zix/ui/common';
+import { CustomIcon } from '@zix/ui/icons';
 import { ZixCookiesBanner, ZixWebFooter, ZixWebHeader } from '@zix/ui/widgets';
 import React, { useEffect, useRef, useState } from 'react';
-import { View } from 'tamagui';
+import { Button, View } from 'tamagui';
 
 export type MainLayoutProps = {
   children: React.ReactNode;
@@ -52,12 +53,26 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
         <ZixWebHeader />
       </View>
       <ZixContainer>
-       {headerVisible && <View height={100}/>}
+        {headerVisible && <View height={100} />}
         {children}
         <ZixCookiesBanner />
       </ZixContainer>
 
       <ZixWebFooter />
+      {!headerVisible && 
+      <Button
+        onPress={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        position='fixed'
+        bottom={'$5'}
+        right={'$5'}
+        zIndex={1}
+        theme={'accent'}
+        padding={'$2'}
+        borderRadius={5}
+        style={{ transform: [{ rotate: '230deg' }] }}
+      >
+        <CustomIcon name='arrow_left' size={20} color='black' />
+      </Button>}
     </View>
   );
 };
