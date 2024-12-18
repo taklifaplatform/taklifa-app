a70ae90d20a57fdeb96a6a4a13c2bdea901bc9f1
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2024-12-18 15:08:01 +0100

    Add dynamic width to ZixSelectField in ZixMonthSelection component

--- libs/ui/forms/src/fields/zix-date-fields/zix-month-selection.tsx
+++ libs/ui/forms/src/fields/zix-date-fields/zix-month-selection.tsx
@@ -2,15 +2,18 @@ import { useMultiLang } from "@zix/i18n"
 import { useMemo } from "react"
 import ZixSelectField, { ZixSelectFieldProps } from "../zix-select-field/zix-select-field"
 import { monthsForLocale } from "./utils"
+import { Dimensions } from "react-native"
 
 export const ZixMonthSelection: React.FC<Partial<ZixSelectFieldProps>> = (props) => {
   const { activeLang } = useMultiLang()
+
+  const SCREEN_WIDTH = Dimensions.get('window').width
 
   const options = useMemo(() => {
     return monthsForLocale({ localeName: activeLang, })
   }, [activeLang])
 
-  return <ZixSelectField {...props} options={options} />
+  return <ZixSelectField {...props} options={options} width={SCREEN_WIDTH / 2}/>
 }
 
 export default ZixMonthSelection
