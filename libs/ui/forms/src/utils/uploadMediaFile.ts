2bfb5ea820ab630b51ab834a5585c2cc94a8391c
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2024-12-23 11:52:16 +0100

    Refactor uploadMediaFile to improve error handling and enhance file processing; add unique file naming and type assignment for images

--- libs/ui/forms/src/utils/uploadMediaFile.ts
+++ libs/ui/forms/src/utils/uploadMediaFile.ts
@@ -16,7 +16,10 @@ export const uploadMediaFile = async (
   file: UploadableMediaFile,
   onProgressUpdate: (progress: number) => void = () => null,
 ): Promise<MediaTransformer> => {
-  if (file?.mediaTypes === MediaTypeOptions.Images || file?.file_type === 'image') {
+  if (
+    file?.mediaTypes === MediaTypeOptions.Images ||
+    file?.file_type === 'image'
+  ) {
     const resizedPhoto = await ImageManipulator.manipulateAsync(
       file.uri,
       [{ resize: { width: 600 } }], // resize to width of 300 and preserve aspect ratio
@@ -24,7 +27,9 @@ export const uploadMediaFile = async (
     );
     file = {
       ...file,
-      uri: resizedPhoto.uri,
+      ...resizedPhoto,
+      file_name: `${randomUUID()}.${ImageManipulator.SaveFormat.JPEG}`,
+      type: 'image/jpeg',
     };
   }
   return new Promise(function (resolve, reject) {
@@ -78,28 +83,35 @@ export const uploadMediaFile = async (
     const formData = new FormData();
 
     // Append the file to the FormData object
-    if (file?.uri?.includes('base64,')) {
-      // Remove metadata from base64 string
-      const base64Data = file.uri.split(',')[1];
-      // Convert base64 to binary
-      const binaryData = atob(base64Data);
-      // Convert binary to ArrayBuffer
-      const arrayBuffer = new ArrayBuffer(binaryData.length);
-      const uint8Array = new Uint8Array(arrayBuffer);
-      for (let i = 0; i < binaryData.length; i++) {
-        uint8Array[i] = binaryData.charCodeAt(i);
+    try {
+      if (file?.uri?.includes('base64,')) {
+        // Remove metadata from base64 string
+        const base64Data = file.uri.split(',')[1];
+        // Convert base64 to binary
+        const binaryData = atob(base64Data);
+        // Convert binary to ArrayBuffer
+        const arrayBuffer = new ArrayBuffer(binaryData.length);
+        const uint8Array = new Uint8Array(arrayBuffer);
+        for (let i = 0; i < binaryData.length; i++) {
+          uint8Array[i] = binaryData.charCodeAt(i);
+        }
+        // Create Blob from ArrayBuffer
+        const blob = new Blob([arrayBuffer], { type: file.mime_type }); // Change type accordingly
+        // Create FormData
+        formData.append('file', blob, file.file_name);
+      } else {
+        formData.append('file', {
+          // uri: ['ios', 'android'].includes(Platform.OS)
+          //   ? file.uri.replace('file://', '')
+          //   : file.uri,
+          uri: Platform.OS === 'ios' ? file.uri.replace('file://', '') : file.uri,
+          name: file.file_name,
+          type: file.type,
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } as any);
       }
-      // Create Blob from ArrayBuffer
-      const blob = new Blob([arrayBuffer], { type: file.mime_type }); // Change type accordingly
-      // Create FormData
-      formData.append('file', blob, file.file_name);
-    } else {
-      formData.append('file', {
-        uri: Platform.OS === 'ios' ? file.uri.replace('file://', '') : file.uri,
-        name: file.file_name,
-        type: file.type,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any);
+    } catch (error) {
+      reject(error);
     }
 
     formData.append('uuid', file.uuid || randomUUID());
