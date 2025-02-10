7450bbfa65cc07d4a7a47dce4e506d1842af7ef5
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2025-02-10 15:57:54 +0100

    Refactor shipment summary screen to use AppLayout and export as a functional component

--- apps/website/pages/app/shipment-manager/[shipment]/summary.tsx
+++ apps/website/pages/app/shipment-manager/[shipment]/summary.tsx
@@ -1,6 +1,15 @@
 import { ShipmentSummaryScreen } from '@zix/features/shipments';
+import { AppLayout } from '@zix/ui/layouts';
 import React from 'react';
 
-export default function Screen() {
-  return <ShipmentSummaryScreen />;
+export const Screen = () => {
+  return (
+    <ShipmentSummaryScreen />
+  )
 }
+
+Screen.getLayout = (Screen) => (
+  <AppLayout>
+    {Screen}
+  </AppLayout>
+)
