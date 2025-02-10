a1686053254e0d56a538fd6431bfcc6801b722c8
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2025-02-10 16:04:26 +0100

    Remove obsolete shipment manager screens and their layouts

--- apps/website/pages/app/shipment-manager-v1/[shipment]/invitations/index.tsx
+++
@@ -1,6 +0,0 @@
-import { ShipmentInvitationsListScreen } from '@zix/features/shipments';
-import React from 'react';
-
-export default function Screen() {
-  return <ShipmentInvitationsListScreen />;
-}
