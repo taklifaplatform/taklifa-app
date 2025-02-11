0f098d9421dc037421139899dec165cde8a40284
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2025-02-11 10:33:42 +0100

    Refactor AppBottomBar to use a custom PlusButton for create-shipment action

--- libs/ui/layouts/src/components/app-bottom-bar/app-bottom-bar.tsx
+++ libs/ui/layouts/src/components/app-bottom-bar/app-bottom-bar.tsx
@@ -1,7 +1,6 @@
 import { Plus } from '@tamagui/lucide-icons';
-import { useAuth } from '@zix/services/auth';
 import { CustomIcon } from '@zix/ui/icons';
-import { Button, Circle, Theme, ThemeableStackProps, XStack, YStack, Text } from 'tamagui';
+import { Button, Theme, ThemeableStackProps, XStack, YStack, Text } from 'tamagui';
 import { useAppBottomBarMenu } from './app-bottom-menu';
 import { useRouter } from 'solito/router';
 import { usePathname } from '@zix/utils';
@@ -10,7 +9,6 @@ import { usePathname } from '@zix/utils';
 export type AppBottomBarProps = ThemeableStackProps & {
   //
 }
-
 /**
  * TODO:
  * make tab title bolder and back color even if active
@@ -19,6 +17,7 @@ export const AppBottomBar: React.FC<AppBottomBarProps> = (props) => {
   const { menuItems } = useAppBottomBarMenu()
   const router = useRouter()
   const activePath = usePathname();
+
   return (
     <XStack
       elevation='$4'
@@ -29,32 +28,36 @@ export const AppBottomBar: React.FC<AppBottomBarProps> = (props) => {
     >
       {
         menuItems.map((item, index) => (
-          <Button
-            unstyled
-            theme={(
-              activePath === item.href
-              || (`${activePath}/` === item.href)
-            ) ? 'accent' : 'default'
-            }
-            onPress={() => router.push(item.href)}
-            flex={1}
-            height='$6'
-            paddingVertical='$2'
-            hoverStyle={{
-              backgroundColor: '$color6',
-            }}
-          >
-            <YStack flex={1} alignItems='center' gap='$2'>
-              <CustomIcon
-                name={item.icon}
-                color='$color5'
-              // color={color}
-              // size={size}
-              />
-              <Text color='$color10'>{item.title}</Text>
-            </YStack>
+          item.name === 'create-shipment' ?
+            PlusButton({
+              size: 20,
+              focused: activePath === item.href,
+              onPress: () => router.push(item.href)
+            }) :
+            <Button
+              unstyled
+              theme={(
+                activePath === item.href
+                || (`${activePath}/` === item.href)
+              ) ? 'accent' : 'default'
+              }
+              onPress={() => router.push(item.href)}
+              flex={1}
+              height='$6'
+              paddingVertical='$2'
+              hoverStyle={{
+                backgroundColor: '$color6',
+              }}
+            >
+              <YStack flex={1} alignItems='center' gap='$2'>
+                <CustomIcon
+                  name={item.icon}
+                  color='$color5'
+                />
+                <Text color='$color10'>{item.title}</Text>
+              </YStack>
 
-          </Button>
+            </Button>
         ))
       }
 
@@ -64,26 +67,25 @@ export const AppBottomBar: React.FC<AppBottomBarProps> = (props) => {
 }
 
 
-const PlusButton = ({ size, focused }) => {
+const PlusButton = ({ size, focused, onPress }) => {
 
   return (
     <Theme name='accent'>
-      <Circle
-        position="absolute"
-        backgroundColor={focused ? '$color5' : '$color9'}
+      <Button
+        unstyled
+        onPress={onPress}
+        height={size + 34}
+        backgroundColor={focused ? '$color6' : '$color1'}
+        borderRadius='100%'
+        alignItems='center'
+        justifyContent='center'
         width={size + 34}
-        height={size + 34}
-      />
-      <YStack
-        position="absolute"
-        justifyContent="center"
-        alignItems="center"
-        animation="quick"
-        pointerEvents="none"
-        height={size + 34}
+        hoverStyle={{
+          backgroundColor: '$color6',
+        }}
       >
         <Plus size={size + 5} />
-      </YStack>
+      </Button>
     </Theme>
   );
 };
