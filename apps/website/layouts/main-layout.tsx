f11ecaa68fd65ebdcf9e5f15a777c4ea3145c491
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2025-02-14 12:47:14 +0100

    Refactor MainLayout component by removing scroll handling logic and simplifying header rendering

--- apps/website/layouts/main-layout.tsx
+++ apps/website/layouts/main-layout.tsx
@@ -1,7 +1,7 @@
 import { ZixContainer } from '@zix/ui/common';
 import { CustomIcon } from '@zix/ui/icons';
 import { ZixCookiesBanner, ZixWebFooter, ZixWebHeader } from '@zix/ui/widgets';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Button, View } from 'tamagui';
 
 export type MainLayoutProps = {
@@ -9,61 +9,15 @@ export type MainLayoutProps = {
 };
 
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  const [headerVisible, setHeaderVisible] = useState(true);
-  const lastScrollY = useRef(0);
-
-  const handleScroll = () => {
-    const currentScrollY = window.scrollY;
-
-    if (currentScrollY > lastScrollY.current) {
-      // Scrolling down
-      // after 100px, hide the header
-      if (currentScrollY > 100) {
-        setHeaderVisible(false);
-      }
-    } else if (currentScrollY < lastScrollY.current) {
-      // Scrolling up
-      // show the header
-      setHeaderVisible(true);
-    }
-
-    lastScrollY.current = currentScrollY;
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const headerStyle = {
-    zIndex: 20,
-    transition: 'transform 0.3s ease-in-out',
-    transform: headerVisible ? 'translateY(0)' : 'translateY(-100%)',
-  };
 
   return (
     <View flex={1} backgroundColor='$color1'>
-      <View
-        position='fixed'
-        zIndex={100}
-        alignSelf='center'
-        top={-10}
-        left={0}
-        right={0}
-        style={headerStyle}>
-        <ZixWebHeader />
-      </View>
+      <ZixWebHeader />
       <ZixContainer>
-        {headerVisible && <View height={100} />}
         {children}
         <ZixCookiesBanner />
       </ZixContainer>
-
       <ZixWebFooter />
-
       <Button
         onPress={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
         position='fixed'
