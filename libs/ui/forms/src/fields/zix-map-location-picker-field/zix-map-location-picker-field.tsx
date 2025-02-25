e88d9a5b3e2e45c623ea091b356c483c5dd68221
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2025-02-25 11:07:51 +0100

    Add dark mode support with custom map style in ZixMapLocationPickerField

--- libs/ui/forms/src/fields/zix-map-location-picker-field/zix-map-location-picker-field.tsx
+++ libs/ui/forms/src/fields/zix-map-location-picker-field/zix-map-location-picker-field.tsx
@@ -27,6 +27,7 @@ import { ZixInput } from '..';
 import ZixMapPointerField from '../zix-map-pointer-field/zix-map-pointer-field';
 import MapView from 'react-native-maps';
 import { AppLayout } from '@zix/ui/layouts';
+import { useThemeSetting } from '@zix/providers';
 
 export type ZixMapLocationPickerFieldProps = {
   value: LocationTransformer;
@@ -313,6 +314,109 @@ export const ZixMapLocationPickerField: React.FC<ZixMapLocationPickerFieldProps>
 }) => {
   const [open, setOpen] = useState(false);
   const [localLocation, setLocalLocation] = useState(value)
+  const { current } = useThemeSetting();
+  const darkMapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [{ "color": "#212121" }]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [{ "visibility": "off" }]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#757575" }]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [{ "color": "#212121" }]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [{ "color": "#757575" }]
+    },
+    {
+      "featureType": "administrative.country",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#9e9e9e" }]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [{ "visibility": "off" }]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#bdbdbd" }]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#757575" }]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [{ "color": "#181818" }]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#616161" }]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.stroke",
+      "stylers": [{ "color": "#1b1b1b" }]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [{ "color": "#2c2c2c" }]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#8a8a8a" }]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [{ "color": "#373737" }]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [{ "color": "#3c3c3c" }]
+    },
+    {
+      "featureType": "road.highway.controlled_access",
+      "elementType": "geometry",
+      "stylers": [{ "color": "#4e4e4e" }]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#616161" }]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#757575" }]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [{ "color": "#000000" }]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [{ "color": "#3d3d3d" }]
+    }
+  ];
 
   return (
     <>
@@ -322,6 +426,7 @@ export const ZixMapLocationPickerField: React.FC<ZixMapLocationPickerFieldProps>
             <MapView
               key={`map-${localLocation?.latitude}-${localLocation?.longitude}`}
               style={{ flex: 1, height: 200, borderRadius: 14 }}
+              customMapStyle={current === 'dark' ? darkMapStyle : []}
               provider='google'
               initialRegion={{
                 latitude: localLocation?.latitude || 24.713552,
