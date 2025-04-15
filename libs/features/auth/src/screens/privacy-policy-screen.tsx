285d6cb025decba7480fe6f19da0b76c7f3bda1c
Choaibe Ifaoui <ifaoui.choaibe@gmail.com>
2025-04-15 11:05:03 +0100

    Update Privacy Polict to match google requirement

--- libs/features/auth/src/screens/privacy-policy-screen.tsx
+++ libs/features/auth/src/screens/privacy-policy-screen.tsx
@@ -1,7 +1,7 @@
 import { AppHeader } from '@zix/ui/layouts';
 import { t } from 'i18next';
 import { Platform } from 'react-native';
-import { H1, H2, Paragraph, ScrollView, YStack, isWeb } from 'tamagui';
+import { H1, H2, Paragraph, ScrollView, View, YStack, isWeb } from 'tamagui';
 
 export const PrivacyPolicyScreen = () => {
   return (
@@ -10,137 +10,64 @@ export const PrivacyPolicyScreen = () => {
         title={t('account:privacy_policy.title')}
         showBackButton
       />}
-      <ScrollView
-        paddingTop="$6"
-      >
+      <ScrollView paddingTop="$6">
         {Platform.OS === 'web' && <AppHeader
           title={t('account:privacy_policy.title')}
           showBackButton
         />}
         <YStack gap="$4" padding="$4">
-          {/* only show title on web since mobile has navigator title */}
           {isWeb && <H1>{t('account:privacy_policy.title')}</H1>}
-          <H2>The Introduction</H2>
+          
+          <H2>Introduction</H2>
           <Paragraph>
-            Sawaeed Inc built the Sawaeed app as a Free app. This SERVICE is
-            provided by Sawaeed Inc at no cost and is intended for use as is.
-            {isWeb ? <br /> : '\n'}
-            This page is used to inform visitors regarding our policies with the
-            collection, use, and disclosure of Personal Information if anyone
-            decided to use our Service.
-            {isWeb ? <br /> : '\n'}
-            If you choose to use our Service, then you agree to the collection and
-            use of information in relation to this policy. The Personal Information
-            that we collect is used for providing and improving the Service. We will
-            not use or share your information with anyone except as described in
-            this Privacy Policy.
-            {isWeb ? <br /> : '\n'}
-            The terms used in this Privacy Policy have the same meanings as in our
-            Terms and Conditions, which are accessible at Sawaeed unless otherwise
-            defined in this Privacy Policy.
+            Sawaeed Inc ("we," "us," or "our") operates the Sawaeed mobile application (the "Service"). 
+            This Privacy Policy explains how we collect, use, disclose, and safeguard your information.
+            {isWeb ? <br /> : '\n\n'}
+            By using Sawaeed, you consent to the practices described in this policy.
           </Paragraph>
 
-          <H2>Information Collection and Use</H2>
+          <H2>Information We Collect</H2>
           <Paragraph>
-            For a better experience, while using our Service, we may require you to
-            provide us with certain personally identifiable information, including
-            but not limited to Sawaeed. The information that we request will be
-            retained by us and used as described in this privacy policy.
+            We may collect:
             {isWeb ? <br /> : '\n'}
-            The app does use third-party services that may collect information used
-            to identify you.
-            {isWeb ? <br /> : '\n'}
-            Link to the privacy policy of third-party service providers used by the
-            app
+            • Personal Data (email, name if provided){isWeb ? <br /> : '\n'}
+            • Usage Data (device info, IP address){isWeb ? <br /> : '\n'}
+            • Log Data (crash reports via third-party tools)
           </Paragraph>
 
-          <H2>Log Data</H2>
+          <H2>Third-Party Services</H2>
           <Paragraph>
-            We want to inform you that whenever you use our Service, in a case of an
-            error in the app we collect data and information (through third-party
-            products) on your phone called Log Data. This Log Data may include
-            information such as your device Internet Protocol (“IP”) address, device
-            name, operating system version, the configuration of the app when
-            utilizing our Service, the time and date of your use of the Service, and
-            other statistics.
-          </Paragraph>
-
-          <H2>Cookies</H2>
-          <Paragraph>
-            Cookies are files with a small amount of data that are commonly used as
-            anonymous unique identifiers. These are sent to your browser from the
-            websites that you visit and are stored on your device's internal memory.
+            We use:
             {isWeb ? <br /> : '\n'}
-            This Service does not use these “cookies” explicitly. However, the app
-            may use third-party code and libraries that use “cookies” to collect
-            information and improve their services. You have the option to either
-            accept or refuse these cookies and know when a cookie is being sent to
-            your device. If you choose to refuse our cookies, you may not be able to
-            use some portions of this Service.
+            • Google Analytics ({isWeb ? <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> : 'Privacy Policy: https://policies.google.com/privacy'}){isWeb ? <br /> : '\n'}
+            • Firebase ({isWeb ? <a href="https://firebase.google.com/support/privacy" target="_blank">Privacy Policy</a> : 'Privacy Policy: https://firebase.google.com/support/privacy'})
           </Paragraph>
 
-          <H2>Service Providers</H2>
+          <H2>Data Security</H2>
           <Paragraph>
-            We may employ third-party companies and individuals due to the following
-            reasons:
-            {isWeb ? <br /> : '\n'}
-            To facilitate our Service;{isWeb ? <br /> : '\n'}
-            To provide the Service on our behalf;{isWeb ? <br /> : '\n'}
-            To perform Service-related services; or{isWeb ? <br /> : '\n'}
-            To assist us in analyzing how our Service is used.
-            {isWeb ? <br /> : '\n'}
-            We want to inform users of this Service that these third parties have
-            access to their Personal Information. The reason is to perform the tasks
-            assigned to them on our behalf. However, they are obligated not to
-            disclose or use the information for any other purpose.
+            We implement security measures, but no method is 100% secure.
           </Paragraph>
 
-          <H2>Security</H2>
+          <H2>Children's Privacy</H2>
           <Paragraph>
-            We value your trust in providing us your Personal Information, thus we
-            are striving to use commercially acceptable means of protecting it. But
-            remember that no method of transmission over the internet, or method of
-            electronic storage is 100% secure and reliable, and we cannot guarantee
-            its absolute security.
+            Our app does not target users under 13. We delete unintended child data.
           </Paragraph>
 
-          <H2>Links to Other Sites</H2>
+          <H2>Changes to This Policy</H2>
           <Paragraph>
-            This Service may contain links to other sites. {isWeb ? <br /> : '\n'}
-            If you click on a third-party link, you will be directed to that site.
-            Note that these external sites are not operated by us. Therefore, we
-            strongly advise you to review the Privacy Policy of these websites. We
-            have no control over and assume no responsibility for the content,
-            privacy policies, or practices of any third-party sites or services.
-          </Paragraph>
-
-          <H2>Children’s Privacy</H2>
-          <Paragraph>
-            These Services do not address anyone under the age of 13.{' '}
-            {isWeb ? <br /> : '\n'}
-            We do not knowingly collect personally identifiable information from
-            children under 13 years of age. In the case we discover that a child
-            under 13 has provided us with personal information, we immediately
-            delete this from our servers. If you are a parent or guardian and you
-            are aware that your child has provided us with personal information,
-            please contact us so that we will be able to do the necessary actions.
-          </Paragraph>
-
-          <H2>Changes to This Privacy Policy</H2>
-          <Paragraph>
-            We may update our Privacy Policy from time to time. Thus, you are
-            advised to review this page periodically for any changes. We will notify
-            you of any changes by posting the new Privacy Policy on this page.
-            {isWeb ? <br /> : '\n'}
-            This policy is effective as of 2024-02-06
+            Last Updated: 2024-02-06{isWeb ? <br /> : '\n'}
+            We'll notify users of changes via app updates.
           </Paragraph>
 
           <H2>Contact Us</H2>
           <Paragraph>
-            If you have any questions or suggestions about our Privacy Policy, do
-            not hesitate to contact us at admin@zixdev.com.
+            For questions or data requests:{isWeb ? <br /> : '\n'}
+            Email: <Paragraph fontWeight="bold">admin@zixdev.com</Paragraph>
           </Paragraph>
         </YStack>
+        <View 
+        height={100}
+        />
       </ScrollView>
     </>
   );
