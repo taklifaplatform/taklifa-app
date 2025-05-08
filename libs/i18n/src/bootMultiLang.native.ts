013dda80cb02b7e3e1de762be0da0200eb90163f
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-05-08 15:53:51 +0100

    Refactor language support in bootMultiLang function; streamline app.json localizations
    - Updated app.json to remove redundant language entries, keeping only "en".
    - Modified bootMultiLang function to support only 'en' and 'ar', adjusting default language logic based on device settings.

--- libs/i18n/src/bootMultiLang.native.ts
+++ libs/i18n/src/bootMultiLang.native.ts
@@ -2,11 +2,12 @@ import { getLocales } from 'expo-localization';
 import { I18nManager } from 'react-native';
 
 export function bootMultiLang() {
-  // TODO: enable this line
-  // const defaultLang = 'en';
-  // const defaultLang = 'ar'
-  const defaultLang = getLocales()?.[0]?.languageCode || 'ar';
+
+  const supportedLangs = ['en', 'ar'];
+  const deviceLang = getLocales()?.[0]?.languageCode || getLocales()?.[0]?.languageTag;
   // alert(defaultLang);
+
+  const defaultLang = !deviceLang || supportedLangs.includes(deviceLang) ? 'ar' : deviceLang;
 
   const isRtl = defaultLang === 'ar';
   I18nManager.allowRTL(isRtl);
