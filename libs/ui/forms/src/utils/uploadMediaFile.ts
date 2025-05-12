56fecf46381644b7abc4a1a65fb16d506abc30f6
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-05-12 09:52:40 +0100

    Update react-hook-form dependency and refactor uploadMediaFile function
    - Downgraded react-hook-form to version 7.51.0 in package.json and package-lock.json for compatibility.
    - Refactored uploadMediaFile function to improve error handling and logging during media uploads.
    - Enhanced the structure of the function for better readability and maintainability.

--- libs/ui/forms/src/utils/uploadMediaFile.ts
+++ libs/ui/forms/src/utils/uploadMediaFile.ts
@@ -26,111 +26,119 @@ export const uploadMediaFile = async (
   onProgressUpdate: (progress: number) => void = () => null,
 ): Promise<MediaTransformer> => {
   return new Promise(function (resolve, reject) {
-    const UPLOAD_URL = `${OpenAPI.BASE}/api/media/uploads`;
+    try {
+      const UPLOAD_URL = `${OpenAPI.BASE}/api/media/uploads`;
 
-    console.log('================');
-    console.log('UPLOADING MEDIA::', file);
-    console.log('UPLOAD_URL::', UPLOAD_URL);
-    console.log('================');
+      console.log('================');
+      console.log('UPLOADING MEDIA::', file);
+      console.log('UPLOAD_URL::', UPLOAD_URL);
+      console.log('================');
 
-    if (!file) {
-      reject({
-        status: 400,
-        statusText: 'No file provided',
-      });
-    }
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', UPLOAD_URL, true);
-    xhr.setRequestHeader('Accept', 'application/json');
-    // auth token
-    xhr.setRequestHeader('Authorization', `Bearer ${OpenAPI.TOKEN}`);
-
-    xhr.upload.addEventListener('progress', (event) => {
-      console.info(
-        'UPLOAD PROGRESS::',
-        JSON.stringify(
-          {
-            loaded: event.loaded,
-            total: event.total,
-            progress: event.loaded / event.total,
-          },
-          null,
-          2,
-        ),
-      );
-      onProgressUpdate(event.loaded / event.total);
-    });
-    xhr.onload = async function () {
-      const data = JSON.parse(xhr.response);
-      console.log('=========');
-      console.log('MEDIA UPLOAD SUCCESS::', data);
-      console.log('=========');
-      if (this.status >= 200 && this.status < 300) {
-        resolve(data);
-      } else {
-        reject(data);
+      if (!file) {
+        reject({
+          status: 400,
+          statusText: 'No file provided',
+        });
       }
-    };
-    xhr.onerror = function (error) {
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', UPLOAD_URL, true);
+      xhr.setRequestHeader('Accept', 'application/json');
+      // auth token
+      xhr.setRequestHeader('Authorization', `Bearer ${OpenAPI.TOKEN}`);
+
+      xhr.upload.addEventListener('progress', (event) => {
+        console.info(
+          'UPLOAD PROGRESS::',
+          JSON.stringify(
+            {
+              loaded: event.loaded,
+              total: event.total,
+              progress: event.loaded / event.total,
+            },
+            null,
+            2,
+          ),
+        );
+        onProgressUpdate(event.loaded / event.total);
+      });
+      xhr.onload = async function () {
+        const data = JSON.parse(xhr.response);
+        console.log('=========');
+        console.log('MEDIA UPLOAD SUCCESS::', data);
+        console.log('=========');
+        if (this.status >= 200 && this.status < 300) {
+          resolve(data);
+        } else {
+          reject(data);
+        }
+      };
+      xhr.onerror = function (error) {
+        console.log('=========');
+        console.log('MEDIA UPLOAD ERROR::', error);
+        console.log('=========');
+        Sentry.captureException(error);
+        // Sentry.(error);
+        reject({
+          status: this.status,
+          statusText: xhr.statusText,
+          error,
+        });
+      };
+
+      // Create a FormData object
+      const formData = new FormData();
+
+      // Append the file to the FormData object
+      try {
+        if (file?.uri?.includes('base64,')) {
+          // Remove metadata from base64 string
+          const base64Data = file.uri.split(',')[1];
+          // Convert base64 to binary
+          const binaryData = atob(base64Data);
+          // Convert binary to ArrayBuffer
+          const arrayBuffer = new ArrayBuffer(binaryData.length);
+          const uint8Array = new Uint8Array(arrayBuffer);
+          for (let i = 0; i < binaryData.length; i++) {
+            uint8Array[i] = binaryData.charCodeAt(i);
+          }
+          // Create Blob from ArrayBuffer
+          const blob = new Blob([arrayBuffer], { type: file.mime_type }); // Change type accordingly
+          // Create FormData
+          formData.append('file', blob, file.file_name);
+        } else {
+          console.log('file.uri', file.uri);
+          console.log('file.file_type', file.file_type);
+          formData.append('file', {
+            // uri: ['ios', 'android'].includes(Platform.OS)
+            //   ? file.uri.replace('file://', '')
+            //   : file.uri,
+            uri: Platform.OS === "ios" ? file.uri.replace("file://", "") : file.uri,
+            // uri:
+            //   Platform.OS === 'ios' ? file.uri.replace('file://', '') : file.uri,
+            name: file.file_name,
+            type: 'image/jpeg',
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          } as any);
+        }
+      } catch (error) {
+        console.log('=========');
+        console.log('MEDIA UPLOAD BUILDING FORM OBJECT ERROR::', error);
+        console.log('=========');
+        Sentry.captureException(error);
+        alert(error?.message);
+        reject(error);
+      }
+
+      formData.append('uuid', file.uuid || randomUUID());
+
+      xhr.send(formData);
+    } catch (error) {
       console.log('=========');
       console.log('MEDIA UPLOAD ERROR::', error);
       console.log('=========');
       Sentry.captureException(error);
-      // Sentry.(error);
-      reject({
-        status: this.status,
-        statusText: xhr.statusText,
-        error,
-      });
-    };
-
-    // Create a FormData object
-    const formData = new FormData();
-
-    // Append the file to the FormData object
-    try {
-      if (file?.uri?.includes('base64,')) {
-        // Remove metadata from base64 string
-        const base64Data = file.uri.split(',')[1];
-        // Convert base64 to binary
-        const binaryData = atob(base64Data);
-        // Convert binary to ArrayBuffer
-        const arrayBuffer = new ArrayBuffer(binaryData.length);
-        const uint8Array = new Uint8Array(arrayBuffer);
-        for (let i = 0; i < binaryData.length; i++) {
-          uint8Array[i] = binaryData.charCodeAt(i);
-        }
-        // Create Blob from ArrayBuffer
-        const blob = new Blob([arrayBuffer], { type: file.mime_type }); // Change type accordingly
-        // Create FormData
-        formData.append('file', blob, file.file_name);
-      } else {
-        console.log('file.uri', file.uri);
-        console.log('file.file_type', file.file_type);
-        formData.append('file', {
-          // uri: ['ios', 'android'].includes(Platform.OS)
-          //   ? file.uri.replace('file://', '')
-          //   : file.uri,
-          uri: Platform.OS === "ios" ? file.uri.replace("file://", "") : file.uri,
-          // uri:
-          //   Platform.OS === 'ios' ? file.uri.replace('file://', '') : file.uri,
-          name: file.file_name,
-          type: 'image/jpeg',
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } as any);
-      }
-    } catch (error) {
-      console.log('=========');
-      console.log('MEDIA UPLOAD BUILDING FORM OBJECT ERROR::', error);
-      console.log('=========');
-      Sentry.captureException(error);
-      alert(error?.message);
       reject(error);
     }
-
-    formData.append('uuid', file.uuid || randomUUID());
-
-    xhr.send(formData);
   });
 };
